e2f64276e72cfcb8f8f4fecdc506169a5c2b4955
Suryansh Bhatnagar <sbh2068@gmail.com>
2023-12-28 19:30:59 +0530

    hardcoded PORT removed

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("./db/conn")
-const PORT = 6005;
+const PORT = process.env.PORT || 6005;
 const session = require("express-session");
 const passport = require("passport");
 const OAuth2Strategy = require("passport-google-oauth2").Strategy;
